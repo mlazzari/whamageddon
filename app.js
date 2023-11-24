a3a90b542e43cbebf289148f63f48ba512e1e9ed
Manuel Lazzari <manuel.lazzari@gmail.com>
2023-11-24 09:18:41 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  updateData();
+});
+
+async function updateData() {
+  try {
+    const players = await fetchPlayers();
+    updatePoints(players);
+    updateRanking(players);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchPlayers() {
+  const response = await fetch('https://your-heroku-app-url/players');
+  const data = await response.json();
+  return data;
+}
+
+function updatePoints(players) {
+  const totalPoints = players.reduce((acc, player) => acc + player.points, 0);
+  document.getElementById("points").textContent = `Points: ${totalPoints}`;
+}
+
+function updateRanking(players) {
+  const rankingList = players.map((player, index) => `${index + 1}. ${player.name} - ${player.points} points`);
+  document.getElementById("ranking").innerHTML = `Ranking: <br>${rankingList.join("<br>")}`;
+}
