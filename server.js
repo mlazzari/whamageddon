61102520eddae1d78f3f54d6599c16dcd92c5768
Manuel Lazzari <manuel.lazzari@gmail.com>
2023-11-24 09:11:01 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB (replace 'your-mongodb-uri' with your actual MongoDB connection string)
+mongoose.connect('your-mongodb-uri', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define a simple Player model
+const Player = mongoose.model('Player', {
+  name: String,
+  points: Number,
+});
+
+// Get all players
+app.get('/players', async (req, res) => {
+  try {
+    const players = await Player.find().sort({ points: -1 });
+    res.json(players);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Add a new player
+app.post('/players', async (req, res) => {
+  try {
+    const { name } = req.body;
+    const player = new Player({ name, points: 0 });
+    await player.save();
+    res.status(201).json(player);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
